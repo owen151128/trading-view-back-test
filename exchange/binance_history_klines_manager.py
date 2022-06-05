f85bf8e294e16562c7964817e82feca0e69509d3
owen151128 <owen151128@gmail.com>
2022-06-05 21:38:44 +0900

    bug(candle): Many candle support(many bug)

--- exchange/binance_history_klines_manager.py
+++ exchange/binance_history_klines_manager.py
@@ -28,7 +28,43 @@ class BinanceHistoryKlinesManager:
         self.start_date = datetime.strptime(start_date, self.time_format)
         self.end_date = datetime.strptime(end_date, self.time_format)
         subtract_time = self.end_date - self.start_date
-        self.candle_count = int(subtract_time.days * 24 + subtract_time.seconds / 3600)
+
+        if self.interval == Client.KLINE_INTERVAL_1HOUR:
+            seconds_division = subtract_time.seconds / 3600
+            self.candle_count = int(subtract_time.days * 24 +
+                                    (seconds_division if subtract_time.seconds % 3600 == 0 else seconds_division + 1))
+        elif self.interval == Client.KLINE_INTERVAL_1MINUTE:
+            seconds_division = subtract_time.seconds / 60
+            self.candle_count = int(subtract_time.days * 1440 +
+                                    (seconds_division if subtract_time.seconds % 60 == 0 else seconds_division + 1))
+        elif self.interval == Client.KLINE_INTERVAL_3MINUTE:
+            days_division = subtract_time.days * 1440 / 3
+            seconds_division = subtract_time.seconds / 180
+            self.candle_count = int(
+                days_division if subtract_time.days * 1440 % 3 == 0 else days_division + 1 + (seconds_division
+                if subtract_time.seconds % 180 == 0 else seconds_division + 1)
+            )
+        elif self.interval == Client.KLINE_INTERVAL_5MINUTE:
+            days_division = subtract_time.days * 1440 / 5
+            seconds_division = subtract_time.seconds / 300
+            self.candle_count = int(
+                days_division if subtract_time.days * 1440 % 5 == 0 else days_division + 1 + (seconds_division
+                if subtract_time.seconds % 300 == 0 else seconds_division + 1)
+            )
+        elif self.interval == Client.KLINE_INTERVAL_15MINUTE:
+            days_division = subtract_time.days * 1440 / 15
+            seconds_division = subtract_time.seconds / 900
+            self.candle_count = int(
+                days_division if subtract_time.days * 1440 % 15 == 0 else days_division + 1 + (seconds_division
+                if subtract_time.seconds % 900 == 0 else seconds_division + 1)
+            )
+        elif self.interval == Client.KLINE_INTERVAL_30MINUTE:
+            days_division = subtract_time.days * 1440 / 30
+            seconds_division = subtract_time.seconds / 1800
+            self.candle_count = int(
+                days_division if subtract_time.days * 1440 % 30 == 0 else days_division + 1 + (seconds_division
+                if subtract_time.seconds % 1800 == 0 else seconds_division + 1)
+            )
 
         # max weight(2400) : limit(candle : 1500, weight : 10) -> max weight(2400) / weight(10) = 240
         # 1500 * 240 = 360000 -> 1m limit : 360000
@@ -37,10 +73,34 @@ class BinanceHistoryKlinesManager:
         self.key_manager = key_manager
 
     def get_klines(self, excel_path: str, is_real_download: bool):
+        # if self.candle_count >= 360000:
+        #     print('[*] Binance limit over!!! fatal error...')
+        #     exit(-1)
         current_start_date = self.start_date.astimezone(timezone.utc)
-        current_end_date = current_start_date + timedelta(
-            hours=self.candle_count if self.candle_count <= 1501 else 1501
-        )
+        if self.interval == Client.KLINE_INTERVAL_1HOUR:
+            current_end_date = current_start_date + timedelta(
+                hours=self.candle_count if self.candle_count <= 1501 else 1501
+            )
+        elif self.interval == Client.KLINE_INTERVAL_1MINUTE:
+            current_end_date = current_start_date + timedelta(
+                minutes=self.candle_count if self.candle_count <= 1501 else 1501
+            )
+        elif self.interval == Client.KLINE_INTERVAL_3MINUTE:
+            current_end_date = current_start_date + timedelta(
+                minutes=self.candle_count / 3 if self.candle_count <= 1501 else 1501 / 3
+            )
+        elif self.interval == Client.KLINE_INTERVAL_5MINUTE:
+            current_end_date = current_start_date + timedelta(
+                minutes=self.candle_count * (5 if self.candle_count <= 1501 else 1501 * 5)
+            )
+        elif self.interval == Client.KLINE_INTERVAL_15MINUTE:
+            current_end_date = current_start_date + timedelta(
+                minutes=self.candle_count * (15 if self.candle_count <= 1501 else 1501 * 15)
+            )
+        elif self.interval == Client.KLINE_INTERVAL_30MINUTE:
+            current_end_date = current_start_date + timedelta(
+                minutes=self.candle_count * (30 if self.candle_count <= 1501 else 1501 * 30)
+            )
         downloaded_candle_count = 0
         binance_client = Client(api_key=self.key_manager.api_key, api_secret=self.key_manager.api_secret_key)
 
@@ -62,10 +122,38 @@ class BinanceHistoryKlinesManager:
                 self.df = pd.concat([self.df, current_df])
 
             downloaded_candle_count += 1500
-            current_start_date = current_end_date + timedelta(hours=1)
-            current_end_date = current_start_date + timedelta(
-                hours=self.candle_count - downloaded_candle_count - i
-                if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+
+            if self.interval == Client.KLINE_INTERVAL_1HOUR:
+                current_start_date = current_end_date + timedelta(hours=1)
+                current_end_date = current_start_date + timedelta(
+                    hours=self.candle_count - downloaded_candle_count - i
+                    if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+            elif self.interval == Client.KLINE_INTERVAL_1MINUTE:
+                current_start_date = current_end_date + timedelta(minutes=1)
+                current_end_date = current_start_date + timedelta(
+                    minutes=self.candle_count - downloaded_candle_count - i
+                    if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+            elif self.interval == Client.KLINE_INTERVAL_3MINUTE:
+                current_start_date = current_end_date + timedelta(minutes=3)
+                current_end_date = current_start_date + timedelta(
+                    minutes=(self.candle_count - downloaded_candle_count - i) * 3
+                    if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+            elif self.interval == Client.KLINE_INTERVAL_5MINUTE:
+                current_start_date = current_end_date + timedelta(minutes=5)
+                current_end_date = current_start_date + timedelta(
+                    minutes=(self.candle_count - downloaded_candle_count - i) * 5
+                    if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+            elif self.interval == Client.KLINE_INTERVAL_15MINUTE:
+                current_start_date = current_end_date + timedelta(minutes=15)
+                current_end_date = current_start_date + timedelta(
+                    minutes=(self.candle_count - downloaded_candle_count - i) * 15
+                    if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+            elif self.interval == Client.KLINE_INTERVAL_30MINUTE:
+                current_start_date = current_end_date + timedelta(minutes=30)
+                current_end_date = current_start_date + timedelta(
+                    minutes=(self.candle_count - downloaded_candle_count - i) * 30
+                    if self.candle_count - downloaded_candle_count - i < 1501 else 1501)
+
             print(f'downloaded candle count : {downloaded_candle_count}')
 
         if is_real_download:
